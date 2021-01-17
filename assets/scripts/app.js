c7f208c511283cb456285cf9cdc278332561c373
shivusoumya <shivadatacamp@gmail.com>
2021-01-17 21:16:20 +0530

    --- Auto-Git Commit ---

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,47 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+// gets inputs from input filed
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
 
-outputResult(currentResult, '');
+// generates and writes calculation log
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); // from vendor file
+}
+
+// 
+function add() {
+    const entereNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult += entereNumber;
+    createAndWriteLog('+', initalResult, entereNumber);
+}
+
+function subtrct() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteLog('-', initalResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteLog('*', initalResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteLog('/', initalResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtrct);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
